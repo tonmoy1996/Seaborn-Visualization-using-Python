79f8431d33323d3e21a64b4db99831bf8b82ec5d
tonmoy1996 <saha.tonmoy1996@gmail.com>
2020-01-09 02:25:31 +0600

    catagorical plot 2nd

--- catagorical plot.py
+++ catagorical plot.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt 
 
 import seaborn as sns 
+import numpy as np
 
 tips= sns.load_dataset('tips')
 # print (tips)
 
 # Bar plot
 
-sns.barplot(x='sex',y='total_bill',data=tips)
+sns.barplot(x='sex',y='total_bill',data=tips,estimator=np.mean)
+
+# count plot
+
+sns.countplot(x="sex",data=tips)
 plt.show()
